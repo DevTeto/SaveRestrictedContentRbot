f4cb160665c2b3e97884747ba23396266ea07c2d
ᴀHᴍᴇᴅ Tᴇᴛᴏ <157909403+DevTeto@users.noreply.github.com>
2024-05-12 04:30:28 +0300

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -5,7 +5,7 @@ from telethon.tl.types import InputMediaPhoto
 
 S = "/start"
 START_PIC = "https://graph.org/file/da97ceca70e55983b4891.png"
-TEXT = "↢ انا هو [بوت حفظ المحتـوي المقيد](https://t.me/wx_pm)\n\nلاستخدامي مـثال : `https://t.me/m_A_s_k33/72501`"
+TEXT = "↢ انـا [بوت حفظ المحتوي المقيد 📥](https://t.me/wx_pm)\n\n-لاستخدامي مـثال : `https://t.me/m_A_s_k33/72501`"
 
 def is_set_button(data):
     return data == "set"
@@ -23,18 +23,18 @@ async def sett(event):
         xx = await conv.send_message("**↢ هذا هو مطوري الرسمي للتواصل @TOPTETO**")
         x = await conv.get_reply()
         if not x.media:
-            xx.edit("No media found.")
+            xx.edit("**↢ هذا هو مطوري الرسمي للتواصل @TOPTETO**")
             return
         mime = x.file.mime_type
         if 'png' not in mime and 'jpg' not in mime and 'jpeg' not in mime:
-            return await xx.edit("No image found.")
+            return await xx.edit("**↢ هذا هو مطوري الرسمي للتواصل @TOPTETO**")
         await xx.delete()
-        t = await event.client.send_message(event.chat_id, 'Trying.')
+        t = await event.client.send_message(event.chat_id, 'جـاري المحاوله.')
         path = await event.client.download_media(x.media)
         if os.path.exists(f'{event.sender_id}.jpg'):
             os.remove(f'{event.sender_id}.jpg')
         os.rename(path, f'./{event.sender_id}.jpg')
-        await t.edit("Temporary thumbnail saved!")
+        await t.edit("انضم هنا فضلا وتواصل مع المطور @M_A_S_K33")
 
 @gagan.on(events.CallbackQuery(pattern=b"rem"))
 async def remt(event):  
